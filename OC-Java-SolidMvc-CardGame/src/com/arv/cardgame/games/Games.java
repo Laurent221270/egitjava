2da2374ade6576f45e602b4ea50a91b0c631e0cf
Buisson <Buisson@Buisson-PC>
2023-09-03 23:41:39 +0200

    reformatage et commentaires

--- OC-Java-SolidMvc-CardGame/src/com/arv/cardgame/games/Games.java
+++ OC-Java-SolidMvc-CardGame/src/com/arv/cardgame/games/Games.java
@@ -3,6 +3,7 @@ package com.arv.cardgame.games;
 import com.arv.cardgame.controller.GameController;
 import com.arv.cardgame.model.DeckFactory;
 import com.arv.cardgame.model.DeckFactory.DeckType;
+import com.arv.cardgame.view.CommandLineView;
 import com.arv.cardgame.view.GameSwingPassiveView;
 import com.arv.cardgame.view.GameSwingView;
 import com.arv.cardgame.view.GameViewables;
@@ -13,19 +14,20 @@ public class Games {
 		
 		GameViewables views = new GameViewables();
 		
+		// Game Main Swing view
 		GameSwingView gsv= new GameSwingView();
 		gsv.createAndShowGUI();
 		
 		views.addViewable(gsv);
 		
-		//passive views
+		// add passive Swing views (Consoles)
 		for (int i = 0; i < 3; i++) {
 			GameSwingPassiveView passiveView= new GameSwingPassiveView();
 			passiveView.createAndShowGUI();
 			
 			views.addViewable(passiveView);
 			
-			// sleep to move new Swing frame on window
+			// sleep to let user move new Swing frame on window
 			try {
 				Thread.sleep(2500);
 			} catch (InterruptedException e) {
@@ -41,15 +43,19 @@ public class Games {
 		gc= new GameController(DeckFactory.makeDeck(DeckType.Normal), gsv, new HighCardGameEvaluator());
 		gc.run();
 		*/
-
 		/*
 		gc= new GameController(DeckFactory.makeDeck(DeckType.Small), gsv, new LowCardGameEvaluator());
 		gc.run();
 		 */
-		
+				
 		gc= new GameController(DeckFactory.makeDeck(DeckType.Test), views, new HighCardGameEvaluator());
 		gc.run();
-		
+			
+		//Jeu en Mode Console
+		/*
+		gc= new GameController(DeckFactory.makeDeck(DeckType.Test), new CommandLineView(), new HighCardGameEvaluator());
+		gc.run();
+		*/
 	}
 
 }
